f176547ff0fd29846673f281ae3a1aab72338be7
Jim Bennett <jim.bennett@microsoft.com>
2020-10-27 12:49:32 -0700

    Revert "Remove pyqt client"
    This reverts commit a49a66153b45f644c0c20f8ca2b5657e3f00c476.

--- remote_control/remote_control/views.py
+++ remote_control/remote_control/views.py
@@ -16,44 +16,19 @@ from picar import back_wheels, front_wheels
 from django.http import HttpResponse
 import picar
 
-is_setup = False
+picar.setup()
+db_file = "/home/pi/SunFounder_PiCar-V/remote_control/remote_control/driver/config"
+fw = front_wheels.Front_Wheels(debug=False, db=db_file)
+bw = back_wheels.Back_Wheels(debug=False, db=db_file)
+cam = camera.Camera(debug=False, db=db_file)
+cam.ready()
+bw.ready()
+fw.ready()
+ 
+SPEED = 60
+bw_status = 0
 
-def setup():
-	global fw, bw, cam, SPEED, bw_status, is_setup
-	if is_setup == True:
-		return
-	from .picar_v_video_stream import Vilib
-	picar.setup()
-	db_file = "/home/pi/SunFounder_PiCar-V/remote_control/remote_control/driver/config"
-	fw = front_wheels.Front_Wheels(debug=False, db=db_file)
-	bw = back_wheels.Back_Wheels(debug=False, db=db_file)
-	cam = camera.Camera(debug=False, db=db_file)
-	cam.ready()
-	bw.ready()
-	fw.ready()
-	
-	Vilib.camera_start()
-
-	SPEED = 60
-	bw_status = 0
-	is_setup = True
-
-#test.start()
-#print(stream.start())
-def run_command(cmd):
-    import subprocess
-    p = subprocess.Popen(
-        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    result = p.stdout.read().decode('utf-8')
-    status = p.poll()
-    return status, result
-
-
-def get_ip():
-    _, result = run_command("hostname -I")
-    ip = result.split(" ")
-    return ip[0]
-
+print(stream.start())
 
 def home(request):
 	return render_to_response("base.html")
@@ -63,10 +38,8 @@ def run(request):
 	debug = ''
 	if 'action' in request.GET:
 		action = request.GET['action']
-		if action == 'setup':
-			setup()
 		# ============== Back wheels =============
-		elif action == 'bwready':
+		if action == 'bwready':
 			bw.ready()
 			bw_status = 0
 		elif action == 'forward':
@@ -116,8 +89,7 @@ def run(request):
 		if bw_status != 0:
 			bw.speed = SPEED
 		debug = "speed =", speed
-	#host = stream.get_host().decode('utf-8').split(' ')[0]
-	host = get_ip()
+	host = stream.get_host().decode('utf-8').split(' ')[0]
 	return render_to_response("run.html", {'host': host})
 
 def cali(request):
